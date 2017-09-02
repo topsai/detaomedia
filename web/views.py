01433d2fe7d41cd376455bc4646705dd1a1dfb6b
topsai <hurte@foxmail.com>
2017-09-02 15:28:16 +0800

    Initial commit

---
+++ web/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def test(request):
+    return render(request, 'test.html')
+
+
+def master(request):
+    return render(request, 'master.html')
