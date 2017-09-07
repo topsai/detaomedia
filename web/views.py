1f8ff80e7d08ae146d7399e4ae241337bd9f7640
topsai <hurte@foxmail.com>
2017-09-07 16:26:54 +0800

    update

--- web/views.py
+++ web/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+import subprocess
 
 # Create your views here.
 def index(request):
@@ -20,3 +20,16 @@ def master(request):
 
 def product(request):
     return render(request, 'product.html')
+
+
+def update(request):
+    if request.method == 'POST':
+        # github的钩子被触发了
+        data = request.POST
+        # log.debug(data)
+        p = subprocess.Popen('. /Arduino/conf/update', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.stdout.encoding = 'utf8'
+        # if p.stdout:
+        #     log.debug('更新成功', p.stdout.read())
+        # else:
+        #     log.debug('更新失败', p.stderr.read())
