f610298c7a6c1a53b4f53fb972156791cd965d76
topsai <hurte@foxmail.com>
2017-09-07 23:03:36 +0800

    update

--- detaomedia/settings.py
+++ detaomedia/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'detaomedia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates") ],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-#TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+# TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'),)
