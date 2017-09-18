37e9c1bec61499a1b805b5db373ffbcd7a0f9b6d
topsai <hurte@foxmail.com>
2017-09-18 09:36:14 +0800

    update

--- web/models.py
+++ web/models.py
@@ -4,4 +4,15 @@ from django.db import models
 # Create your models here.
 
 class Master(models.Model):
+    # 大师编号，用于排序
     sorting = models.IntegerField()
+    # 头像
+    avatar = models.CharField(max_length=256)
+    # 中文名
+    chinese_name = models.CharField(max_length=256)
+    # 本名
+    actual_name = models.CharField(max_length=256)
+    # 简介
+    introduction = models.CharField(max_length=256)
+    content = models.CharField(max_length=10000)
+
