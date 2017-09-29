11ebb5e3ffceabdfb4df96bc65c2316be30412f1
topsai <hurte@foxmail.com>
2017-09-29 17:21:09 +0800

    update

---
+++ background/forms/article.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from django.core.exceptions import ValidationError
+from django import forms as django_forms
+from django.forms import fields as django_fields
+from django.forms import widgets as django_widgets
+
+from web import models
+
+
+class MasterForm(django_forms.Form):
+    sorting = django_fields.IntegerField(
+        widget=django_widgets.NumberInput(attrs={'class': 'form-control', 'placeholder': '大师序号'}, )
+    )
+    avatar = django_fields.CharField(
+        widget=django_widgets.TextInput(attrs={'class': 'form-control', 'placeholder': '大师头像'})
+        # widget=django_widgets.
+    )
+    chinese_name = django_fields.CharField(
+        widget=django_widgets.TextInput(attrs={'class': 'form-control', 'placeholder': '中文名', 'rows': '3'})
+    )
+    actual_name = django_fields.CharField(
+        widget=django_widgets.TextInput(attrs={'class': 'form-control', 'placeholder': '本名', })
+    )
+    field = django_fields.MultipleChoiceField(
+        widget=django_widgets.Select(choices=models.Field.objects.all())
+    )
+    nationality = django_fields.MultipleChoiceField(
+        widget=django_widgets.Select()
+    )
+    introduction = django_fields.CharField(
+        widget=django_widgets.TextInput(attrs={'class': 'form-control', 'placeholder': '简介', 'rows': '3'})
+    )
+    content = django_fields.CharField(
+        widget=django_widgets.Textarea(attrs={'class': 'kind-content'})
+    )
+
+
+class FieldForm(django_forms.Form):
+    field = django_fields.CharField(
+        widget=django_widgets.TextInput(attrs={'class': 'form-control', 'placeholder': '领域', }),
+    )
+
+
+class NationalityForm(django_forms.Form):
+    nationality = django_fields.CharField(
+        widget=django_widgets.TextInput(attrs={'class': 'form-control', 'placeholder': '国籍', })
+    )
---
+++ background/urls.py
@@ -0,0 +1,32 @@
+"""detaomedia URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from background import views
+
+urlpatterns = [
+    url(r'^index.html/$', views.index),
+    url(r'^field.html$', views.field),
+    url(r'^index.html$', views.index),
+    url(r'^base-info.html/$', views.base_info),
+    url(r'^tag.html/$', views.tag),
+    url(r'^category.html/$', views.category),
+    url(r'^article-(?P<article_type_id>\d+)-(?P<category_id>\d+).html/$', views.article, name='article'),
+    url(r'^add-article.html/$', views.add_article),
+    url(r'^add-field.html/$', views.add_field),
+    url(r'^edit-article-(?P<nid>\d+).html/$', views.edit_article),
+    url(r'^upload-avatar.html/$', views.upload_avatar),
+]
